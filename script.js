394c5244cc39d83b9a8d817f28b5a927f7016075
Breno Romeiro <bsromeiro@gmail.com>
2022-10-17 21:21:31 -0300

    basic layout

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("script works!!");
+gsap.fromTo(
+  "#first, #second",
+  { backdropFilter: "brightness(.9)" },
+  {
+    backdropFilter: "brightness(0)",
+    ease: "none",
+    scrollTrigger: {
+      start: "top bottom",
+      trigger: "#second",
+      end: "top top",
+      scrub: true,
+    },
+  }
+);
+
+gsap.to("#first-block", {
+  y: -200,
+  scrollTrigger: {
+    start: "top bottom",
+    trigger: "#second",
+    end: "top top",
+    scrub: 1.5,
+  },
+});
