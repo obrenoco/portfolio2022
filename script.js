f6bfd66bfc82b40f14eecc8a7beb23702bb20abc
Breno Romeiro <bsromeiro@gmail.com>
2022-10-19 17:40:36 -0300

    add first page

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-gsap.fromTo(
-  "#first, #second",
-  { backdropFilter: "brightness(.9)" },
-  {
-    backdropFilter: "brightness(0)",
-    ease: "none",
-    scrollTrigger: {
-      start: "top bottom",
-      trigger: "#second",
-      end: "top top",
-      scrub: true,
-    },
-  }
-);
+gsap.to("#fake-markee", { x: -600, duration: 20, repeat: -1, ease: "linear" });
 
-gsap.to("#first-block", {
-  y: -200,
-  scrollTrigger: {
-    start: "top bottom",
-    trigger: "#second",
-    end: "top top",
-    scrub: 1.5,
-  },
-});
+// gsap.fromTo(
+//   "#first, #second",
+//   { backdropFilter: "brightness(.9)" },
+//   {
+//     backdropFilter: "brightness(0)",
+//     ease: "none",
+//     scrollTrigger: {
+//       start: "top bottom",
+//       trigger: "#second",
+//       markers: true,
+//       end: "top 80%",
+//       scrub: true,
+//     },
+//   }
+// );
+
+// gsap.to("#first-block", {
+//   y: -200,
+//   scrollTrigger: {
+//     start: "top bottom",
+//     trigger: "#second",
+//     end: "top top",
+//     scrub: 1.5,
+//   },
+// });
